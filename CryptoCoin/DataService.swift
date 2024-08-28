bebcd2a2264ac600acea76930cf17bc13bb23687
Valerii Pavlikov <valerypavlikov@yandex.ru>
2024-08-28 18:40:13 +0100

    - removed API_KEY & config file

--- CryptoCoin/DataService.swift
+++ CryptoCoin/DataService.swift
@@ -10,16 +10,9 @@ import Foundation
 
 struct DataService {
     
-    let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
-    
     func getCoinList() async -> [Coin] {
-        guard apiKey != nil else {
-            return [Coin]()
-        }
-        
         if let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=rub") {
             var request = URLRequest(url: url)
-            request.addValue("Bearer \(apiKey!)", forHTTPHeaderField: "Authorization")
             request.addValue("application/json", forHTTPHeaderField: "accept")
             
             do {

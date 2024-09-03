3abf02bcc0cd9dda2b10b8c1e81a654716a6f831
Valerii Pavlikov <valerypavlikov@yandex.ru>
2024-09-03 12:47:56 +0100

    - add 3 types of btns to select a list of coins

--- CryptoCoin/MainView.swift
+++ CryptoCoin/MainView.swift
@@ -16,24 +16,41 @@ struct MainView: View {
         
         @Bindable var coinModel = coinModel
         
+        HStack {
+            Button("Get data by async/await") {
+                coinModel.getCoinList()
+            }
+            .padding()
+            
+            Button("Get data by closures") {
+                print("Closures")
+            }
+            .padding()
+            
+            Button("Get data by Combine") {
+                print("Combine")
+            }
+            .padding()
+        }
+        .disabled(!coinModel.coinList.isEmpty)
+        
+        Button("Clean the List") {
+            coinModel.coinList = [Coin]()
+        }
+        .disabled(coinModel.coinList.isEmpty)
+        .foregroundStyle(coinModel.coinList.isEmpty ? .gray : .red)
+        .padding()
+        
         VStack {
             List(coinModel.coinList) { coin in
                 Text(coin.name ?? "")
                     .onTapGesture {
                         coinModel.selectedCoin = coin
-                         coordinator.present(sheet: .coinDetail)
+                        coordinator.present(sheet: .coinDetail)
                     }
             }
             .listStyle(.plain)
         }
         .padding()
-        .onAppear {
-            coinModel.getCoinList()
-        }
     }
-}
-
-#Preview {
-    MainView()
-        .environment(CoinModel())
 }
---
+++ CryptoCoin/PLayground.swift
@@ -0,0 +1,8 @@
+//
+//  PLayground.swift
+//  CryptoCoin
+//
+//  Created by Valerie N. Prinz on 30/08/2024.
+//
+
+import Foundation
